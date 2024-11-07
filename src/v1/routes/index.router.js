d8e30ace06196f5791a961717cac57ac5f9a0a81
Hoang Do Gia <hoangdg268.tb@email.com>
2024-11-07 16:53:45 +0700

    update api 5

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -5,7 +5,20 @@ router.get('/checkstatus', (req, res, next) => {
     res.status(200).json({
         status: 'success',
         message: 'api ok',
-
+        metadata: [
+            {
+                name: "Hoang 1",
+                age: 99
+            },
+            {
+                name: "Hoang 2",
+                age: 98
+            },
+            {
+                name: "Hoang 2",
+                age: 97
+            },
+        ]
     })
 })
 router.get('/api/users', (req, res, next) => {
